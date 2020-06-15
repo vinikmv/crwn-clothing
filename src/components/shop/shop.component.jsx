637bb85babfd246ab3ebeddc3c0726783994f0ad
Vinicius Vicente <viniciuskmv@gmail.com>
2020-06-15 11:42:07 -0300

    update shop page, collection preview e collection item component

---
+++ src/components/shop/shop.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import SHOP_DATA from "./shop.data.js";
+
+import "../../components/collection-preview/collection-preview.component.jsx";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component.jsx";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...OtherCollectionProps }) => (
+          <CollectionPreview key={id} {...OtherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
