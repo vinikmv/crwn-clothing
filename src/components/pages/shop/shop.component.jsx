774d7d20674fe29ab156c7ca0a957209047cf2f7
Vinicius Vicente <viniciuskmv@gmail.com>
2020-06-27 13:32:20 -0300

    test

--- src/components/pages/shop/shop.component.jsx
+++ src/components/pages/shop/shop.component.jsx
@@ -34,7 +34,7 @@ class ShopPage extends React.Component {
       this.setState({ loading: false });
     });
   }
-
+  //
   render() {
     const { match } = this.props;
     const { loading } = this.state;
