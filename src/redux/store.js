9db9f5c55468dbd4d068e810f1ef1b2559e1d228
Vinicius Vicente <viniciuskmv@gmail.com>
2020-07-02 18:50:55 -0300

    aula 194

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,22 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./root-reducer";
+import rootSaga from "./root-saga";
 
-export const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
+
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
