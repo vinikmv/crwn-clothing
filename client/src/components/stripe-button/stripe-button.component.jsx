0210d7b4851508b4da57c6cf60572994e5d32244
Vinicius Vicente <viniciuskmv@gmail.com>
2020-07-08 10:53:28 -0300

    Separado o app em client e server, adicionado um ExpressServer para pagamentos

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51GxYDTB3O0rqklhfsSKOHkFopzmu1XO8o83nMrGHROxmXT2onb1oHgzbscWBAUQpPWv6SuxwmzrlPVugQ5TJt7wq00OGd6QKB5";
+
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment Successful");
+      })
+      .catch((error) => {
+        console.log("Payment Error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please sure you use the provided credit card"
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
